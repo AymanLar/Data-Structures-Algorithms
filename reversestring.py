79a61efdd7643e40338924759202756a455cb682
Namya <larourayman@gmail.com>
2021-07-24 16:03:24 +0100

    stringrevers

---
+++ reversestring.py
@@ -0,0 +1,12 @@
+'''txt = 'jdijf'
+print(txt[::-1])'''
+
+# we do a lil trollin
+
+def qleb_chqleb(s):
+    return s[::-1]
+
+input_lol = 'كل في فلك '
+
+if __name__ == '__main__':
+    print('the reverse is =  '  , qleb_chqleb(input_lol))
