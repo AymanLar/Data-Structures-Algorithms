346ce734987e27fbecd3309535afc8c08d59ead8
Namya <larourayman@gmail.com>
2021-07-27 01:22:20 +0100

    factorial

---
+++ calculfactorial.py
@@ -0,0 +1,13 @@
+num = 5
+facto = 1
+if num< 0:
+    print("i beg u pardon dummass" )
+elif num == 0 :
+    print('1')
+else:
+    for i in range(1, num+1 ):
+        facto = facto*i
+    print('its' , facto)
+
+
+
