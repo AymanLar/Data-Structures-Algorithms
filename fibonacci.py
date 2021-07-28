074c7cb95f7789f8a34244951c83cd243d8e78bf
Namya <larourayman@gmail.com>
2021-07-29 00:05:28 +0100

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -11,16 +11,39 @@ while terms > count :
     n1=n2
     n2 =nth
     count = count +1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
+'''
+def fibo(n):
+    if n>= 1:
+        print(fibo(n-1)+fibo(n-2))
 
+terms = 5
 
-
-
-
-
-
-
-
-
-
-
+if terms >= 0:
+    print('nums are')
+    for i in range(terms):
+        print(fibo(i))
+'''
