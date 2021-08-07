869d034e43428db0bfe33512f5f40fdd8deabcf6
Namya <larourayman@gmail.com>
2021-08-07 22:01:18 +0100

    draw

---
+++ drawpatts.py
@@ -0,0 +1,47 @@
+rows = 6 
+b = 0
+
+for i in range(rows, 0,-1):
+    b = b+1 
+    for j in range(i):
+        print(b, end ='     ')
+
+    print('')
+
+py = 'python'
+x =''
+for k in py:
+    x = x+k
+    print(x)
+
+
+wor = 'blad vs bladiya nonono'
+u = ''
+for m in wor :
+    u = u +m
+
+    print(u)
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ noise.py
@@ -0,0 +1,9 @@
+
+import winsound 
+winsound.Beep(2000, 600)
+winsound.Beep(1000, 100)
+winsound.Beep(2000, 400)
+winsound.Beep(2000, 500)
+winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+winsound.PlaySound("beep.wav", winsound.SND_FILENAME)
+
