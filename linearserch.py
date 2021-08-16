0713fae40fad4fbe475d597be149b2a9bbcd270b
Namya <larourayman@gmail.com>
2021-08-16 18:04:19 +0100

    Linear Search

---
+++ linearserch.py
@@ -0,0 +1,16 @@
+
+
+
+"""
+list = [1,2,3,6,9, 5]
+target = 6
+
+"""
+
+def linearsearch(array , target):
+    for i in range(len(array)):
+        if list[i] == target :
+            return i
+        else:
+            return -1
+
