b91a86f783aab05ae3b99dda286590cbfb8902d1
Ayman <larourayman@gmail.com>
2021-10-29 21:25:14 +0100

    reverse an Array

---
+++ reversanArray.py
@@ -0,0 +1,13 @@
+def reverseArr(arr,s ,e ):
+    # s = 0  start of arr 
+    # e = n-1  end of arr 
+    while s < e :
+        arr[s], arr[e] = arr[e] , arr[s]
+        s +=1
+        e -= 1
+
+arr = [7, 9, 0, 4, 2]
+print(arr)
+reverseArr(arr, 0, 4)
+print("Reversed list is")
+print(arr)
