09587037b05feb46e31c5ce9f48f76be90f855c9
Ayman <larourayman@gmail.com>
2021-10-30 20:20:09 +0100

    Get arr as input from user

---
+++ userarrinput.py
@@ -0,0 +1,11 @@
+arr = [] # empty array
+
+
+size = int(input('enter the size of the arr : '))
+
+for i in range(size): 
+    element = int(input("enter the num : "))
+    
+    arr.append(element) # adding the element
+        
+print(arr)
